808ee648f7d4395e78b45ae5e83dc10a1ed4a654
Phil Gyford <phil@gyford.com>
2014-07-24 11:34:52 +0100

    Remove debug logging

--- js/mappiness.ui.general.js
+++ js/mappiness.ui.general.js
@@ -87,9 +87,7 @@ function($,        jquery_modal,   d3) {
     exports.loaderShow = function() { 
       $('#loader').show();
       // If this starts taking a while, we show an extra message:
-      console.log('a');
       loaderTimeout = setTimeout(function(){
-        console.log('show');
         $('#loader-slow').fadeIn(1000); 
       }, 3000);
     };
